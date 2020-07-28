10fd4b680fb0f5256bb04daf8bbae3f391c10518
Víctor <donvictor@gmail.com>
2020-07-28 19:00:16 +0200

    Extracted amounts chart into a separate Dart file.

--- lib/screens/summary_screen.dart
+++ lib/screens/summary_screen.dart
@@ -8,9 +8,7 @@ import '../models/account.dart';
 import '../tools/number_formatting.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import '../tools/constants.dart';
-import 'package:fl_chart/fl_chart.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
-import 'dart:math';
+import 'package:indexa_dashboard/widgets/amounts_chart.dart';
 
 const int nbsp = 0x00A0;
 
@@ -52,18 +50,6 @@ class _SummaryScreenState extends State<SummaryScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final List<Color> color = <Color>[];
-    color.add(Colors.blue[50]);
-    color.add(Colors.blue);
-
-
-    final List<double> stops = <double>[];
-    stops.add(0.0);
-    stops.add(0.5);
-
-    final LinearGradient gradientColors =
-    LinearGradient(transform: GradientRotation(pi*1.5), colors: color, stops: stops);
-
     return Scaffold(
 //      appBar: AppBar(
 //        title: Text(
@@ -274,68 +260,7 @@ class _SummaryScreenState extends State<SummaryScreen> {
 //                                child: LineChart(
 //                                  amountsChartData(snapshot.data.amountsSeries),
 //                                ),
-                                    child: SfCartesianChart(
-                                      margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                                        primaryYAxis: NumericAxis(
-                                            labelFormat: '{value} €'),
-                                        trackballBehavior: TrackballBehavior(
-                                          enable: true,
-                                          activationMode: ActivationMode.singleTap,
-                                          tooltipDisplayMode: TrackballDisplayMode.groupAllPoints,
-                                          tooltipAlignment: ChartAlignment.near,
-                                          tooltipSettings: InteractiveTooltip(
-                                            enable: true,
-                                            decimalPlaces: 2,
-                                          ),
-                                        ),
-                                        palette: <Color>[
-                                          Colors.blue,
-                                          Colors.blueGrey,
-                                        ],
-//                                        tooltipBehavior: TooltipBehavior(
-//                                          enable: true,
-//                                          shared: true,
-//                                          decimalPlaces: 2,
-//                                        ),
-                                        legend: Legend(
-                                            isVisible: true,
-                                            position: LegendPosition.top,
-                                        padding: 4,
-                                        itemPadding: 10),
-                                        // Initialize DateTime axis
-                                        primaryXAxis: DateTimeAxis(),
-                                        series: <
-                                            ChartSeries<AmountsDataPoint,
-                                                DateTime>>[
-                                          AreaSeries<AmountsDataPoint,
-                                              DateTime>(
-                                            name: 'Total',
-                                            opacity: 0.7,
-                                            // Bind data source
-                                            dataSource:
-                                                snapshot.data.amountsSeries,
-                                            xValueMapper:
-                                                (AmountsDataPoint amounts, _) =>
-                                                    amounts.date,
-                                            yValueMapper:
-                                                (AmountsDataPoint amounts, _) =>
-                                                    amounts.totalAmount,
-                                            gradient: gradientColors,
-                                          ),
-                                          LineSeries<AmountsDataPoint,
-                                              DateTime>(
-                                            name: 'Aportado',
-                                            // Bind data source
-                                            dataSource:
-                                                snapshot.data.amountsSeries,
-                                            xValueMapper:
-                                                (AmountsDataPoint amounts, _) =>
-                                                    amounts.date,
-                                            yValueMapper:
-                                                (AmountsDataPoint amounts, _) =>
-                                                    amounts.netAmount,
-                                          ),
-                                        ]),
+                                    child: AmountsChart(amountsSeries: snapshot.data.amountsSeries),
                                   ),
                                 ),
                                 SizedBox(
---
+++ lib/widgets/amounts_chart.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:indexa_dashboard/models/account.dart';
+import 'dart:math';
+import 'package:indexa_dashboard/models/amounts_datapoint.dart';
+
+class AmountsChart extends StatelessWidget {
+  const AmountsChart({
+    Key key,
+    //@required this.gradientColors,
+    @required this.amountsSeries,
+  }) : super(key: key);
+
+  //final LinearGradient gradientColors;
+  final List<AmountsDataPoint> amountsSeries;
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Color> color = <Color>[];
+    color.add(Colors.blue[50]);
+    color.add(Colors.blue);
+
+
+    final List<double> stops = <double>[];
+    stops.add(0.0);
+    stops.add(0.5);
+
+    final LinearGradient gradientColors =
+    LinearGradient(transform: GradientRotation(pi*1.5), colors: color, stops: stops);
+
+    return SfCartesianChart(
+        margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        primaryYAxis: NumericAxis(
+            labelFormat: '{value} €'),
+        trackballBehavior: TrackballBehavior(
+          enable: true,
+          activationMode: ActivationMode.singleTap,
+          tooltipDisplayMode: TrackballDisplayMode.groupAllPoints,
+          tooltipAlignment: ChartAlignment.near,
+          tooltipSettings: InteractiveTooltip(
+            enable: true,
+            decimalPlaces: 2,
+          ),
+        ),
+        palette: <Color>[
+          Colors.blue,
+          Colors.blueGrey,
+        ],
+        legend: Legend(
+            isVisible: true,
+            position: LegendPosition.top,
+            padding: 4,
+            itemPadding: 10),
+        // Initialize DateTime axis
+        primaryXAxis: DateTimeAxis(),
+        series: <
+            ChartSeries<AmountsDataPoint,
+                DateTime>>[
+          AreaSeries<AmountsDataPoint,
+              DateTime>(
+            name: 'Total',
+            opacity: 0.7,
+            // Bind data source
+            dataSource:
+            amountsSeries,
+            xValueMapper:
+                (AmountsDataPoint amounts, _) =>
+            amounts.date,
+            yValueMapper:
+                (AmountsDataPoint amounts, _) =>
+            amounts.totalAmount,
+            gradient: gradientColors,
+          ),
+          LineSeries<AmountsDataPoint,
+              DateTime>(
+            name: 'Aportado',
+            // Bind data source
+            dataSource:
+            amountsSeries,
+            xValueMapper:
+                (AmountsDataPoint amounts, _) =>
+            amounts.date,
+            yValueMapper:
+                (AmountsDataPoint amounts, _) =>
+            amounts.netAmount,
+          ),
+        ]);
+  }
+}
