7c09d8e2c7ceebb252b1f461aaedce9dd3b640ef
Víctor <donvictor@gmail.com>
2020-08-19 02:10:37 +0200

    Tweaked design of login screen. Pending help tooltip on how to get API token.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:indexa_dashboard/screens/root_screen.dart';
@@ -148,7 +149,8 @@ class _LoginScreenState extends State<LoginScreen> {
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(
-        builder: (BuildContext context) => RootScreen(token: tokenTextController.text),
+        builder: (BuildContext context) =>
+            RootScreen(token: tokenTextController.text),
       ),
     );
   }
@@ -164,61 +166,176 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                TextField(
-                  controller: tokenTextController,
-                  keyboardType: TextInputType.text,
-                  maxLines: null,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'API token',
-                    hintText: 'Tu token de Indexa',
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 100),
+                        child: Center(
+                          child: Image.asset('assets/images/indexa_logo.png'),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                MaterialButton(
-                  child: Text(
-                    'ENTRAR',
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child:
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                Container(),
+                                InkWell(
+                                  onTap: () {
+                                    print('tap');
+                                  },
+                                  child: Row(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                                        child: Text(
+                                          'Cómo obtener token',
+                                          style: TextStyle(
+                                            color: Colors.black38,
+                                          ),
+                                        ),
+                                      ),
+                                      Icon(
+                                        Icons.help_outline,
+                                        color: Colors.black38,
+                                        size: 20,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                      ),
+                      TextField(
+                        controller: tokenTextController,
+                        keyboardType: TextInputType.text,
+                        maxLines: null,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'API token',
+                          hintText: 'Tu token de Indexa',
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 20),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.lock_outline,
+                                  color: Colors.black54,
+                                ),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text(
+                                  'Recordar token',
+                                  style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Switch(
+                            value: rememberToken,
+                            onChanged: (newValue) {
+                              if (newValue) {
+                                enableRememberToken();
+                              } else {
+                                disableRememberToken();
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+//                SwitchListTile(
+//                  title: Text(
+//                    'Recordar token',
+//                    style: TextStyle(
+//                      color: Colors.black54
+//                    ),
+//                  ),
+//                  value: rememberToken,
+//                  //controlAffinity: ListTileControlAffinity.leading,
+//                  secondary: Icon(
+//                    Icons.lock_outline,
+//                  ),
+//                  //dense: true,
+//                  contentPadding:
+//                      EdgeInsets.fromLTRB(0, 0, 0, 0),
+//                  onChanged: (newValue) {
+//                    if (newValue) {
+//                      enableRememberToken();
+//                    } else {
+//                      disableRememberToken();
+//                    }
+//                  },
+//                ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      SizedBox(
+                        width: 60,
+                        height: 60,
+                        child: MaterialButton(
+                          child: Icon(
+                            Icons.arrow_forward,
+                            color: Colors.white,
+                            //size: 30,
+                          ),
+                          //height: 60,
+                          padding: EdgeInsets.all(0),
+                          color: Colors.blue,
+                          textColor: Colors.white,
+                          elevation: 8,
+                          onPressed: () {
+                            if (rememberToken) {
+                              saveTokenAndGoToHome();
+                            } else {
+                              goToHome();
+                            }
+                          },
+                          shape: CircleBorder(),
+                        ),
+                      ),
+                    ],
                   ),
-                  height: 40,
-                  color: Colors.blue,
-                  textColor: Colors.white,
-                  elevation: 8,
-                  onPressed: () {
-                    if (rememberToken) {
-                      saveTokenAndGoToHome();
-                    } else {
-                      goToHome();
-                    }
-                  },
-                ),
-                CheckboxListTile(
-                  title: Text(
-                    'Recordar token',
-                    textAlign:
-                        Platform.isIOS ? TextAlign.left : TextAlign.right,
+                    ],
                   ),
-                  value: rememberToken,
-                  controlAffinity: ListTileControlAffinity.platform,
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 20, horizontal: 0),
-                  onChanged: (newValue) {
-                    if (newValue) {
-                      enableRememberToken();
-                    } else {
-                      disableRememberToken();
-                    }
-                  },
-                ),
-              ],
+//                MaterialButton(
+//                  child: Text(
+//                    'ENTRAR',
+//                  ),
+//                  height: 45,
+//                  color: Colors.blue,
+//                  textColor: Colors.white,
+//                  elevation: 8,
+//                  onPressed: () {
+//                    if (rememberToken) {
+//                      saveTokenAndGoToHome();
+//                    } else {
+//                      goToHome();
+//                    }
+//                  },
+//                ),
+
             ),
           ),
         ),
