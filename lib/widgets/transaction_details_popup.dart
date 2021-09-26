fccfc0fc379d4d65f54ceeccb857e4d11495f73c
Víctor Marino <6GitHubVaMal9>
2021-09-26 17:21:57 +0200

    Refactored code for the transaction details pop-up in a separate class file.

---
+++ lib/widgets/transaction_details_popup.dart
@@ -0,0 +1,149 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:indexa_dashboard/tools/constants.dart';
+
+class TransactionDetailsPopup extends StatelessWidget {
+  const TransactionDetailsPopup({
+    Key key,
+    @required this.transactionData,
+  }) : super(key: key);
+
+  final transactionData;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> transactionDetails = [];
+
+    transactionDetails.add(Text(
+      "Cuenta:",
+      style: kTransactionDetailTitleTextStyle,
+    ));
+    transactionDetails.add(Text(
+      transactionData.accountType,
+      style: kTransactionDetailValueTextStyle,
+    ));
+    transactionDetails.add(Text(
+      "Concepto:",
+      style: kTransactionListTitleTextStyle,
+    ));
+    transactionDetails.add(Text(
+      transactionData.operationType,
+      style: kTransactionDetailValueTextStyle,
+    ));
+    transactionDetails.add(
+      Text(
+        "Fecha operación:",
+        style: kTransactionListTitleTextStyle,
+      ),
+    );
+
+    transactionDetails.add(
+      Text(
+        DateFormat("dd/MM/yyyy").format(transactionData.date),
+        style: kTransactionDetailValueTextStyle,
+      ),
+    );
+    if (transactionData.accountType == "Cuenta de valores") {
+      transactionDetails.add(Text(
+        "Fecha valor:",
+        style: kTransactionListTitleTextStyle,
+      ));
+      transactionDetails.add(Text(
+        DateFormat("dd/MM/yyyy").format(transactionData.valueDate),
+        style: kTransactionDetailValueTextStyle,
+      ));
+      transactionDetails.add(Text(
+        "Fecha fiscal:",
+        style: kTransactionListTitleTextStyle,
+      ));
+      transactionDetails.add(Text(
+        DateFormat("dd/MM/yyyy").format(transactionData.fiscalDate),
+        style: kTransactionDetailValueTextStyle,
+      ));
+    }
+
+    transactionDetails.add(
+      Divider(),
+    );
+
+    if (transactionData.accountType == "Cuenta de valores") {
+      transactionDetails.add(Text(
+        "Fondo:",
+        style: kTransactionListTitleTextStyle,
+      ));
+      transactionDetails.add(Text(
+        transactionData.instrumentName,
+        style: kTransactionDetailValueTextStyle,
+      ));
+      transactionDetails.add(Text(
+        "ISIN:",
+        style: kTransactionListTitleTextStyle,
+      ));
+      transactionDetails.add(Text(
+        transactionData.instrumentCode,
+        style: kTransactionDetailValueTextStyle,
+      ));
+      transactionDetails.add(Text(
+        "Participaciones:",
+        style: kTransactionListTitleTextStyle,
+      ));
+      transactionDetails.add(Text(
+        transactionData.titles.toString(),
+        style: kTransactionDetailValueTextStyle,
+      ));
+      transactionDetails.add(Text(
+        "Valor liquidativo:",
+        style: kTransactionListTitleTextStyle,
+      ));
+      transactionDetails.add(Text(
+        transactionData.price.toString() + " €",
+        style: kTransactionDetailValueTextStyle,
+      ));
+      transactionDetails.add(Text(
+        "Coste:",
+        style: kTransactionListTitleTextStyle,
+      ));
+      transactionDetails.add(Text(
+        transactionData.amount.toString() + " €",
+        style: kTransactionDetailValueTextStyle,
+      ));
+      transactionDetails.add(
+        Divider(),
+      );
+    }
+    transactionDetails.add(
+      Text(
+        "Estado:",
+        style: kTransactionListTitleTextStyle,
+      ),
+    );
+    transactionDetails.add(
+      Text(
+        transactionData.status,
+        style: kTransactionDetailValueTextStyle,
+      ),
+    );
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20)),
+      title: Text(
+        "Detalles",
+        //style: kTransactionListAmountTextStyle,
+      ),
+      content: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: transactionDetails,
+      ),
+      contentPadding: EdgeInsets.fromLTRB(24, 24, 24, 0),
+      actions: [
+        TextButton(
+          child: Text('OK'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/transaction_tile.dart
+++ lib/widgets/transaction_tile.dart
@@ -5,6 +5,7 @@ import 'package:indexa_dashboard/tools/constants.dart';
 import 'package:indexa_dashboard/tools/number_formatting.dart';
 import 'package:indexa_dashboard/models/transaction.dart';
 import 'package:indexa_dashboard/widgets/reusable_card.dart';
+import 'package:indexa_dashboard/widgets/transaction_details_popup.dart';
 
 class TransactionTile extends StatelessWidget {
   const TransactionTile({
@@ -40,7 +41,9 @@ class TransactionTile extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 5),
                 child: Text(
-                  DateFormat("MMMM y").format(transactionData.date).toUpperCase(),
+                  DateFormat("MMMM y")
+                      .format(transactionData.date)
+                      .toUpperCase(),
                   style: kDividerTextStyle,
                 ),
               ),
@@ -51,236 +54,71 @@ class TransactionTile extends StatelessWidget {
       );
     }
 
-    List<Widget> transactionDetails = [];
-
-    transactionDetails.add(
-        Text(
-          "Cuenta:",
-              style: kTransactionDetailTitleTextStyle,
-        )
-    );
-    transactionDetails.add(
-        Text(
-          transactionData.accountType,
-          style: kTransactionDetailValueTextStyle,
-        )
-    );
-    transactionDetails.add(
-        Text(
-          "Concepto:",
-          style: kTransactionListTitleTextStyle,
-        )
-    );
-    transactionDetails.add(
-        Text(
-          transactionData.operationType,
-          style: kTransactionDetailValueTextStyle,
-        )
-    );
-    transactionDetails.add(
-      Text(
-        "Fecha operación:",
-        style: kTransactionListTitleTextStyle,
-      ),
-    );
-
-    transactionDetails.add(
-      Text(
-        DateFormat("dd/MM/yyyy").format(transactionData.date),
-        style: kTransactionDetailValueTextStyle,
-      ),
-    );
-    if (transactionData.accountType == "Cuenta de valores") {
-      transactionDetails.add(
-          Text(
-            "Fecha valor:",
-            style: kTransactionListTitleTextStyle,
-          )
-      );
-      transactionDetails.add(
-          Text(
-            DateFormat("dd/MM/yyyy").format(transactionData.valueDate),
-            style: kTransactionDetailValueTextStyle,
-          )
-      );
-      transactionDetails.add(
-          Text(
-            "Fecha fiscal:",
-            style: kTransactionListTitleTextStyle,
-          )
-      );
-      transactionDetails.add(
-          Text(
-            DateFormat("dd/MM/yyyy").format(transactionData.fiscalDate),
-            style: kTransactionDetailValueTextStyle,
-          )
-      );
-    }
-
-    transactionDetails.add(
-      Divider(),
-    );
-
-    if (transactionData.accountType == "Cuenta de valores") {
-      transactionDetails.add(
-          Text(
-            "Fondo:",
-            style: kTransactionListTitleTextStyle,
-          )
-      );
-      transactionDetails.add(
-          Text(
-            transactionData.instrumentName,
-            style: kTransactionDetailValueTextStyle,
-          )
-      );
-      transactionDetails.add(
-          Text(
-            "ISIN:",
-            style: kTransactionListTitleTextStyle,
-          )
-      );
-      transactionDetails.add(
-          Text(
-            transactionData.instrumentCode,
-            style: kTransactionDetailValueTextStyle,
-          )
-      );
-      transactionDetails.add(
-          Text(
-            "Participaciones:",
-            style: kTransactionListTitleTextStyle,
-          )
-      );
-      transactionDetails.add(
-          Text(
-            transactionData.titles.toString(),
-            style: kTransactionDetailValueTextStyle,
-          )
-      );
-      transactionDetails.add(
-          Text(
-            "Valor liquidativo:",
-            style: kTransactionListTitleTextStyle,
-          )
-      );
-      transactionDetails.add(
-          Text(
-            transactionData.price.toString() + " €",
-            style: kTransactionDetailValueTextStyle,
-          )
-      );
-      transactionDetails.add(
-          Text(
-            "Coste:",
-            style: kTransactionListTitleTextStyle,
-          )
-      );
-      transactionDetails.add(
-          Text(
-            transactionData.amount.toString() + " €",
-            style: kTransactionDetailValueTextStyle,
-          )
-      );
-      transactionDetails.add(
-        Divider(),
-      );
-    }
-    transactionDetails.add(
-      Text(
-        "Estado:",
-        style: kTransactionListTitleTextStyle,
-      ),
-    );
-    transactionDetails.add(
-      Text(
-        transactionData.status,
-        style: kTransactionDetailValueTextStyle,
-      ),
-    );
     tileElements.add(
-        InkWell(
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Icon(
-                  transactionData.icon,
-                  color: Colors.blueAccent,
-                  size: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        transactionData.operationType,
-                        textAlign: TextAlign.left,
-                        style: kTransactionListTitleTextStyle,
-                      ),
-                      RichText(
-                        text: TextSpan(children: [
-                          TextSpan(
-                            text: DateFormat("dd/MM").format(transactionData.date).replaceAll(".", ""),
-                            //textAlign: TextAlign.left,
-                            style: kCardSubTextStyle.copyWith(
-                              fontSize: 12,
-                            ),
-                          ),
-                          TextSpan(
-                            text: " · " + transactionData.accountType,
-                            //textAlign: TextAlign.left,
-                            style: kCardSubTextStyle.copyWith(
-                              fontSize: 12,
-                            ),
-                          ),
-                        ]),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Text(
-                    transactionData.amount.toStringAsFixed(2) + " €",
-                    textAlign: TextAlign.right,
-                    style: kTransactionListAmountTextStyle.copyWith(fontWeight: FontWeight.normal),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          onTap: () {
-            showDialog(
-                context: context,
-                builder: (BuildContext context) =>
-                    AlertDialog(
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                        title: Text(
-                          "Detalles",
-                          //style: kTransactionListAmountTextStyle,
-                        ),
-                        content: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children:
-                          transactionDetails,
+      InkWell(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(
+                transactionData.icon,
+                color: Colors.blueAccent,
+                size: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      transactionData.operationType,
+                      textAlign: TextAlign.left,
+                      style: kTransactionListTitleTextStyle,
                     ),
-                      contentPadding: EdgeInsets.fromLTRB(24, 24, 24, 0),
-                      actions: [
-                        TextButton(
-                          child: Text('OK'),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
+                    RichText(
+                      text: TextSpan(children: [
+                        TextSpan(
+                          text: DateFormat("dd/MM")
+                              .format(transactionData.date)
+                              .replaceAll(".", ""),
+                          //textAlign: TextAlign.left,
+                          style: kCardSubTextStyle.copyWith(
+                            fontSize: 12,
+                          ),
                         ),
-                      ],            ),
-            );
-          },
+                        TextSpan(
+                          text: " · " + transactionData.accountType,
+                          //textAlign: TextAlign.left,
+                          style: kCardSubTextStyle.copyWith(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ]),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  transactionData.amount.toStringAsFixed(2) + " €",
+                  textAlign: TextAlign.right,
+                  style: kTransactionListAmountTextStyle.copyWith(
+                      fontWeight: FontWeight.normal),
+                ),
+              ),
+            ],
+          ),
         ),
+        onTap: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => TransactionDetailsPopup(transactionData: transactionData),
+          );
+        },
+      ),
     );
-      return Column(
+    return Column(
       children: tileElements,
     );
   }
