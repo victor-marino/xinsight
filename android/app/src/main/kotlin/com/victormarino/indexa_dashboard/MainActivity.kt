d19983d1dcba2dae226981c2ea41c41b3587027d
Víctor Marino <victor.marino@vodafone.com>
2021-12-25 02:45:39 +0100

    Made changes to migrate to Flutter 1.12+. Improved login flow to better capture the right errors and exceptions.

--- android/app/src/main/kotlin/com/victormarino/indexa_dashboard/MainActivity.kt
+++ android/app/src/main/kotlin/com/victormarino/indexa_dashboard/MainActivity.kt
@@ -1,11 +1,6 @@
 package com.victormarino.indexa_dashboard
 
 import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterFragmentActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
 }
