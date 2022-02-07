8be0c5617963293da9f89c6c20a2f81b1f5e999c
Víctor Marino <victor.marino@vodafone.com>
2022-02-08 00:31:25 +0100

    Added vibration to pull to refresh action

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 import 'screens/login_screen.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:provider/provider.dart';
 import 'tools/bottom_navigation_bar_provider.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 void main() {
   runApp(
@@ -24,19 +26,22 @@ class MyApp extends StatelessWidget {
         return BottomNavigationBarProvider();
       },
       child: Center(
-        child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            // This makes the visual density adapt to the platform that you run
-            // the app on. For desktop platforms, the controls will be smaller and
-            // closer together (more dense) than on mobile platforms.
-            visualDensity: VisualDensity.adaptivePlatformDensity,
+        child: RefreshConfiguration(
+          enableRefreshVibrate: true,
+          child: MaterialApp(
+            title: 'Indexa X',
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+              // This makes the visual density adapt to the platform that you run
+              // the app on. For desktop platforms, the controls will be smaller and
+              // closer together (more dense) than on mobile platforms.
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            localizationsDelegates: context.localizationDelegates,
+            supportedLocales: context.supportedLocales,
+            locale: context.locale,
+            home: LoginScreen(),
           ),
-          localizationsDelegates: context.localizationDelegates,
-          supportedLocales: context.supportedLocales,
-          locale: context.locale,
-          home: LoginScreen(),
         ),
       ),
     );
--- lib/screens/overview_screen.dart
+++ lib/screens/overview_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import '../models/account.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
