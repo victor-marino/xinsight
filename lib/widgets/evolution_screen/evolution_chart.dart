af141351a301cbef0119e250fe2115842eeecc1c
Víctor Marino <victor.marino@vodafone.com>
2023-10-31 18:33:47 +0100

    Fix merge conflict between branches

--- lib/widgets/evolution_screen/evolution_chart.dart
+++ lib/widgets/evolution_screen/evolution_chart.dart
@@ -8,7 +8,8 @@ import 'package:indexax/tools/styles.dart' as text_styles;
 import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:indexax/tools/private_mode_provider.dart';
-
+import 'package:indexax/tools/evolution_chart_provider.dart';
+import 'package:indexax/models/chart_series_type.dart';
 
 // Draws the evolution chart
 // It plots the amounts (€) or the returns (%) based on the 'seriesType' variable of the provider
@@ -83,7 +84,7 @@ class EvolutionChart extends StatelessWidget {
             borderWidth: 1,
             textStyle:
                 TextStyle(color: Theme.of(context).colorScheme.onSurface),
-            format: showReturns
+            format: seriesType == ChartSeriesType.returns
                 ? 'series.name: point.y%'
                 : context.watch<PrivateModeProvider>().privateModeEnabled
                     ? 'series.name: ${getAmountAsStringWithZeroDecimals(100, maskValue: true)}'
